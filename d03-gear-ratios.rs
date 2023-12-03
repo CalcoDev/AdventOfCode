df3c1ccc8e951b98bae58bb0aa6fdc958f875937
Calco Dev <calcopoddev@gmail.com>
2023-12-04 00:44:51 +0200

    Day 3. Part 1. (very very very bad code)

--- d03-gear-ratios.rs
+++ d03-gear-ratios.rs
@@ -12,29 +12,29 @@ fn get_coords_idx(idx: usize, width: usize) -> (usize, usize) {
     (idx % width, idx / width)
 }
 
-fn read_num_backward(mut str: &str, idx: usize) -> u32 {
-    str = &str[..idx+1];
-    let prev = str.rfind(|c: char| !c.is_numeric()).unwrap_or(0);
+fn read_num(str: &str, idx: usize) -> (u32, usize, usize) {
+    let prev = if let Some(idx) = str[..idx+1].rfind(|c: char| !c.is_numeric()) {
+        idx as i32
+    } else {
+        -1
+    };
     
-    str[prev+1..].parse().expect("ERROR: Could not parse number to u32!")
-}
+    let next = if let Some(idx) = str[idx+1..].find(|c: char| !c.is_numeric()) {
+        idx as i32
+    } else {
+        str.len() as i32 - 1
+    };
 
-fn read_num_forward(mut str: &str, idx: usize) -> u32 {
-    str = &str[idx..];
-    let next = str.find(|c: char| !c.is_numeric()).unwrap_or(0);
+    let prev = (prev+1) as usize;
+    let next = (idx as i32+next+1) as usize;
+    let num = str[prev..next].parse()
+        .expect("ERROR: Could not parse number to u32!");
     
-    str[..next].parse().expect("ERROR: Could not parse number to u32!")
-}
-
-fn read_num(str: &str, idx: usize) -> u32 {
-    let prev = str[..idx+1].rfind(|c: char| !c.is_numeric()).unwrap_or(0);
-    let next = str[idx+1..].find(|c: char| !c.is_numeric()).unwrap_or(0);
-
-    str[prev+1..idx+next+1].parse().expect("ERROR: Could not parse number to u32!")
+    (num, prev, next)
 }
 
 fn part1() {
-    let input = fs::read_to_string("inputs/own/d03-gear-ratios")
+    let input = fs::read_to_string("inputs/d03-gear-ratios")
         .expect("ERROR: Could not read input!");
 
     let width = if let Some(idx) = input.find("\n") {
@@ -44,23 +44,37 @@ fn part1() {
     };
     let height = input.lines().count();
 
+    let mut nums: Vec<u32> = Vec::new();
     let mut s_idx = 0;
-    let mut sum = 0;
+    while let Some(mut idx) = input.as_str()[s_idx..].find(|c: char| c.is_numeric()) {
+        idx += s_idx;
+        s_idx = idx;
+
+        let next = input.as_str()[s_idx..].find(|c: char| !c.is_numeric()).unwrap_or(0);
+        let num: u32 = input.as_str()[s_idx..s_idx+next].parse()
+            .expect("ERROR: Could not parse number to u32!");
+
+        s_idx += next;
+        nums.push(num);
+    }
+    nums.sort();
+    let all_sum: u32 = nums.iter().sum();
+
+    let mut check: Vec<bool> = vec![false; width * height];
+
+    let mut s_idx = 0;
     while let Some(mut idx) = input.as_str()[s_idx..].find(is_valid_char) {
         idx += s_idx;
         s_idx = idx + 1;
+
         let (x, y) = get_coords_idx(idx, width);
 
-        // println!("STRING: \n{}", &input.as_str()[s_idx..]);
-        // println!("X, Y: {} {}", x + 1, y + 1);
-        // continue;
-
-        for y_off in -1i32..1i32 {
-            for x_off in -1i32..1i32 {
+        for y_off in -1i32..2i32 {
+            for x_off in -1i32..2i32 {
                 if x as i32 + x_off < 0 ||
-                    x as i32 + x_off > width as i32 ||
+                    x as i32 + x_off >= width as i32 ||
                     y as i32 + y_off < 0 ||
-                    y as i32 + y_off > height as i32 {
+                    y as i32 + y_off >= height as i32 {
                     continue;
                 }
                 
@@ -69,26 +83,33 @@ fn part1() {
                     ((y as i32) + y_off) as usize,
                     width
                 );
+                if check[idx] {
+                    continue;
+                }
 
-                sum += if (input.as_bytes()[idx] as char).is_numeric() {
-                    if x_off < 0 {
-                        eprintln!("XOFF < 0");
-                        read_num_backward(input.as_str(), idx)
-                    } else if x_off > 0 {
-                        eprintln!("XOFF > 0");
-                        read_num_forward(input.as_str(), idx)
-                    } else {
-                        eprintln!("XOFF == 0");
-                        read_num(input.as_str(), idx)
+                let calc_x = ((x as i32) + x_off) as usize; 
+                let calc_y = ((y as i32) + y_off) as usize;
+                
+                if (input.as_bytes()[idx] as char).is_numeric() {
+                    let (num, l, r) = read_num(input.as_str(), idx);
+                    if let Ok(idx) = nums.binary_search(&num) {
+                        nums.remove(idx);
+                        for i in l..r {
+                            check[i] = true;
+                        }
                     }
-                } else {
-                    0u32
                 }
             }
         }
     }
-    
+
+    let diff: u32 = nums.iter().sum();
+    let sum = all_sum - diff;
     println!("{}", sum);
+
+    eprintln!("all {} sum {} vec {:?}", all_sum, sum, nums);
+
+    panic!();
 }
 
 fn main() {
